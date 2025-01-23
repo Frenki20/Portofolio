b03408a0a9a564a78dbc93a8cf1c8d0e589a1840
Frenki20 <frenki.idrizi112@gmail.com>
2025-01-23 09:40:21 +0100

    Update Frenki.js

--- PROJEKTIWEB/Frenki.js
+++ PROJEKTIWEB/Frenki.js
@@ -6,15 +6,6 @@
             navMenu.classList.toggle('active');
         });
 
-        // Smooth scroll for navigation links
-        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-            anchor.addEventListener('click', function (e) {
-                e.preventDefault();
-                document.querySelector(this.getAttribute('href')).scrollIntoView({
-                    behavior: 'smooth'
-                });
-            });
-        });
 
            // Timeline animation
         function revealTimeline() {
